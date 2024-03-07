5562bf43c1826466572101a1f46f96ffef1b1c1b
AnderssonSand <143388777+AnderssonSand@users.noreply.github.com>
2024-03-07 15:08:48 +0100

    added a section for utförda bokningar

--- cleaning-service-js-2/src/components/bookingpagecomponents/BookingForm.tsx
+++ cleaning-service-js-2/src/components/bookingpagecomponents/BookingForm.tsx
@@ -152,6 +152,11 @@ const BookingForm: React.FC<IBookingForm> = () => {
                 )}
             </section>
 
+            <section>
+                <h2 className='text-h2 text-primaryBlue ustify-center w-1/2 mx-auto mt-20 mb-5'>Utförda bokningar</h2>
+                
+            </section>
+
         </>
     )
 }
