57f5f7292c68431d331d159924fd3ec06bdf19e3
varamara <mikahvaarama@gmail.com>
2024-03-18 18:36:35 +0100

    Refactor login page and update database

--- cleaning-service-js-2/src/components/loginPageComponents/LoginForm.tsx
+++ cleaning-service-js-2/src/components/loginPageComponents/LoginForm.tsx
@@ -1,25 +1,14 @@
-import { useContext, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { IUser } from "../../interfaces";
-import { RegistrationContext } from "../../contexts/registrationContext/RegistrationContext";
+import React, { useState } from "react";
 
-const registrationForm: React.FC = () => {
-  const { registerUser } = useContext(RegistrationContext) as unknown as {
-    registerUser: (newUser: IUser) => void;
-  };
-
+const LoginForm = () => {
   const [formData, setFormData] = useState({
     username: "",
-    password: "",
-    firstName: "",
-    lastName: "",
     email: "",
+    password: "",
     confirmPassword: "",
   });
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
-  ) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData((prevState) => ({
       ...prevState,
@@ -29,82 +18,20 @@ const registrationForm: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-
-    const newUser: IUser = {
-      id: uuidv4(),
-      username: formData.username,
-      password: formData.password,
-      firstName: formData.firstName,
-      lastName: formData.lastName,
-      email: formData.email,
-    };
-
     console.log(formData);
-    registerUser(newUser);
-    alert("Inloggning lyckades!");
-    setFormData({
-      username: "",
-      password: "",
-      firstName: "",
-      lastName: "",
-      email: "",
-      confirmPassword: "",
-    });
   };
 
   return (
     <>
-   
       <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <h2 className="text-center text-3xl font-extrabold text-gray-900">
-            Register an Account
+          <h2 className="text-center text-3xl font-extrabold text-primaryBlue">
+            Logga in
           </h2>
         </div>
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <form className="space-y-6" onSubmit={handleSubmit}>
-              <div>
-                <label
-                  htmlFor="firstName"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  First Name
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="firstName"
-                    name="firstName"
-                    type="text"
-                    autoComplete="given-name"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
-
-              <div>
-                <label
-                  htmlFor="lastName"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Last Name
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="lastName"
-                    name="lastName"
-                    type="text"
-                    autoComplete="family-name"
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
               <div>
                 <label
                   htmlFor="username"
@@ -190,9 +117,8 @@ const registrationForm: React.FC = () => {
                   type="submit"
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Register
+                  Log in
                 </button>
-                <p className="mt-6 text-sm text-center">Har du redan en användare? <a className="font-medium text-primaryOrange hover:text-indigo-500" href="/login">Logga in</a></p>
               </div>
             </form>
           </div>
@@ -202,4 +128,4 @@ const registrationForm: React.FC = () => {
   );
 };
 
-export default registrationForm;
+export default LoginForm;
