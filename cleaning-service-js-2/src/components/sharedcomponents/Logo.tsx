66835fcad038510ed57946335a4532d979b5f431
AnderssonSand <143388777+AnderssonSand@users.noreply.github.com>
2024-03-18 11:59:08 +0100

    Update  styling on landing page

--- cleaning-service-js-2/src/components/sharedcomponents/Logo.tsx
+++ cleaning-service-js-2/src/components/sharedcomponents/Logo.tsx
@@ -2,7 +2,7 @@ import LogoImg from '../../assets/logoImg.png';
 
 const Logo = () => {
   return (
-    <div className="w-24 h-auto">
+    <div className="w-36 h-auto">
       <img src={LogoImg} alt="Logo" className="w-full h-full" />
     </div>
   );
